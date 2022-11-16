6ee22bd5443223a2b8e5010195c26b2170c68a15
Fatemeh Askari <faskari3108@gmail.com>
2022-11-16 19:21:23 +0330

    Update movie.views

--- movie/views.py
+++ movie/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import Movie
 
 
 def movies_list(request):
     movies = Movie.objects.all()
-    return render(request, 'movies_list.html', context={'movies': movies})
+    return render(request, 'movie/movies_list.html', context={'movies': movies})
+
+
+def movie_detail(request, pk):
+    return HttpResponse(f'<h1>this is movie{pk}</h1>')
