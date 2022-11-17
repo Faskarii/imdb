606b57c67d2d46cc0e0eefed91d33501079749a1
Fatemeh Askari <faskari3108@gmail.com>
2022-11-17 19:45:10 +0330

    Change movie-app's name to movies

--- imdb/urls.py
+++ imdb/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('movie.urls'))
+    path('', include('movies.urls'))
 ]
 
 if settings.DEBUG:
