d0f35eb29e9f70e9aaa0d8a6ca05e8e47229ab92
k-roozbahani <kavehrozbah@gmail.com>
2022-11-22 14:10:01 +0330

    finish models user and add admin and settings

--- imdb/settings.py
+++ imdb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'movies',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
+AUTH_USER_MODEL = "users.User"
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_join', 'is_active', 'is_staff', 'is_superuser']
+    list_filter = ['is_active', 'is_staff']
+
