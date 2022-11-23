00647be7b2decd223622a0b57249f0021303073f
Fatemeh Askari <faskari3108@gmail.com>
2022-11-23 20:38:15 +0330

    Add url and root for static and media

--- imdb/settings.py
+++ imdb/settings.py
@@ -119,8 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
