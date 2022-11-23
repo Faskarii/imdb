b455561b3c640b6e930eb9be9425e1d750022b03
Fatemeh Askari <faskari3108@gmail.com>
2022-11-23 20:36:36 +0330

    Add url and view for movie-add-form

--- movies/views.py
+++ movies/views.py
@@ -1,18 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.shortcuts import render, HttpResponse
 from .models import Movie
-
+from .forms import MovieForm
 
 def movies_list(request):
     limit = int(request.GET.get('limit', 8))
     offset = int(request.GET.get('offset', 0))
     movies = Movie.objects.all()
-    return render(request, 'movie_list.html', context={'movies': movies})
+    return render(request, 'movies/movie_list.html', context={'movies': movies})
 
 
 def movies_detail(request, pk):
     if request.method == 'GET':
         movie = get_object_or_404(Movie, pk=pk, is_valid=True)
         content = {'movie': movie}
-        return render(request, 'movie_detail.html', context=content)
+        return render(request, 'movies/movie_detail.html', context=content)
 
+
+def movies_add(request):
+    if request.method == 'GET':
+        form = MovieForm()
+        return render(request, 'movies/movie_add.html', {'form':form})
+
+    elif request.method == 'POST':
+        form = MovieForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data.get('title')
+            description = form.cleaned_data.get('description')
+            release_date = form.cleaned_data.get('release_date')
+            avatar = form.cleaned_data.get(avatar)
+            Movie.objects.create(
+                title = title,
+                description = description,
+                release_date = release_date,
+                avatar = avatar
+            )
+            return redirect('movies/movie_list.html')
+        return HttpResponse(str(form.errors))
