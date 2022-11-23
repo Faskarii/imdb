6abbf7393e3c5dcb1994c8c742027fab3c53351f
Fatemeh Askari <faskari3108@gmail.com>
2022-11-23 18:13:24 +0330

    Update

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,9 @@
-# <<<<<<< HEAD
 from django.shortcuts import render, get_object_or_404
-# =======
 from django.shortcuts import render, HttpResponse
-# >>>>>>> 606b57c67d2d46cc0e0eefed91d33501079749a1
 from .models import Movie
 
 
 def movies_list(request):
-# <<<<<<< HEAD
     limit = int(request.GET.get('limit', 8))
     offset = int(request.GET.get('offset', 0))
     movies = Movie.objects.all()
@@ -19,11 +15,4 @@ def movies_detail(request, pk):
         movie = get_object_or_404(Movie, pk=pk, is_valid=True)
         content = {'movie': movie}
         return render(request, 'movie_detail.html', context=content)
-# =======
-#     movies = Movie.objects.all()
-#     return render(request, 'movie/movies_list.html', context={'movies': movies})
 
-
-# def movie_detail(request, pk):
-#     return HttpResponse(f'<h1>this is movie{pk}</h1>')
-# >>>>>>> 606b57c67d2d46cc0e0eefed91d33501079749a1
