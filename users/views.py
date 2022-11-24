97a68b08a3442722ac9784141dc14b3f27bb11ba
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2022-11-24 21:48:20 +0330

    finish signup

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .models import User
+from .forms import UserSignUpForm
 
 
 def user_login(request):
@@ -16,7 +17,22 @@ def user_login(request):
         if user:
             login(request, user)
             print()
-            return HttpResponseRedirect('/')
+            return redirect('/')
 
         else:
             return render(request, 'login.html', {'auth_error': True})
+
+
+def user_sign_up(request):
+    if request.method == 'GET':
+        form = UserSignUpForm()
+        context = {'form': form}
+        return render(request, 'signup.html', context)
+    elif request.method == 'POST':
+        form = UserSignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return render(request, 'signup.html', {'form': form})
+
