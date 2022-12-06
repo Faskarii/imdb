925a3e6deb069b4594e7334db77cb147eafa9b51
K-Roozbahani <110715636+K-Roozbahani@users.noreply.github.com>
2022-12-06 15:54:31 +0330

    complate movie api

--- movies/API/serializers.py
+++ movies/API/serializers.py
@@ -5,23 +5,41 @@ from movies.models import Genre, Role, Crew, MovieCrew, Movie
 class GenreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Genre
+        fields = ('id', 'title', 'is_valid', 'created_time', 'modified_time')
 
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
+        fields = ('id', 'title', 'is_valid', 'created_time', 'modified_time')
 
 
 class CrewSerializer(serializers.ModelSerializer):
+    gender = serializers.SerializerMethodField()
+
     class Meta:
         model = Crew
+        fields = ('id', 'first_name', 'last_name', 'birthday', 'gender',
+                  'avatar', 'is_valid', 'created_time', 'modified_time')
+
+    def get_gender(self, obj):
+        return obj.get_gender_display
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True)
+
     class Meta:
         model = Movie
+        fields = ('id', 'title', 'description', 'release_date', 'avatar',
+                  'genres', 'is_valid', 'created_time', 'modified_time')
 
 
 class MovieCrewSerializer(serializers.ModelSerializer):
+    movie = MovieSerializer()
+    crew = CrewSerializer()
+    role = RoleSerializer()
+
     class Meta:
         model = MovieCrew
+        fields = ('id', 'movie', 'crew', 'role', 'created_time', 'modified_time')
